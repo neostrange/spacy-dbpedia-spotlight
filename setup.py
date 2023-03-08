a1bca831c5f6e978073856fe0d68277baaa1c537
Ali Hur <ali.hur57@gmail.com>
2023-03-08 18:15:48 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 def setup_package():
-    setup(name="spacy_dbpedia_spotlight",
+    setup(name="spacy_dbpedia_spotlight_v2",
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown')
